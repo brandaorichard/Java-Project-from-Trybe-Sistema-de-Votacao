b8a1afd7d108e0974c2ab245ed534dab44f51f1e
brandaorichard <rih.brandao@gmail.com>
2024-05-08 05:17:40 -0300

    feat: class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,29 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+    private int numero;
+    private int votos;
 
+    public PessoaCandidata(String nome, int numero) {
+        super();
+        this.nome = nome;
+        this.numero = numero;
+        this.votos = 0;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void receberVoto() {
+        votos++;
+    }
 }
