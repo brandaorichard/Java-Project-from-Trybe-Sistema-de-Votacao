e30260ca96e07f4b377781e7cfd5554f2c9c89a5
brandaorichard <rih.brandao@gmail.com>
2024-05-08 21:24:19 -0300

    feat: class GerenciamentoVotacao & class Principal

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,80 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * Voting Management Class which is an inheritance of Management Votacao Interface.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private final ArrayList<PessoaCandidata> pessoasCandidatas;
+  private final ArrayList<PessoaEleitora> pessoasEleitoras;
+  private final ArrayList<String> cpfsComputados;
+
+  /**
+   * Constructor of the Voting Management class.
+   */
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata novaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(novaCandidata);
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora eleitora : pessoasEleitoras) {
+      if (eleitora.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora novaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(novaEleitora);
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numeroPessoaCandidata) {
+        candidata.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+        return;
+      }
+    }
+    System.out.println("Número da pessoa candidata não encontrado!");
+  }
+
+  @Override
+  public void mostrarResultado() {
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+      return;
+    }
+
+    int totalVotos = cpfsComputados.size();
+    System.out.println("Resultado da votação:");
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      int votos = candidata.getVotos();
+      double percentual = (double) votos / totalVotos * 100;
+      System.out.println("Nome: "
+          + candidata.getNome() + " - " + votos + " votos ( " + Math.round(percentual) + "% )");
+    }
+    System.out.println("Total de votos: " + totalVotos);
+  }
 }
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,10 +1,95 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
+/**
+ * Principal Class.
+ */
 public class Principal {
 
+  /**
+   * Main method.
+   *
+   * @param args Principal object args
+   *
+   */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+    GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
 
+    System.out.println("Cadastro de pessoas candidatas:");
+    while (true) {
+      System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.print("Entre com o número correspondente à opção desejada: ");
+      int opcao = scanner.nextInt();
+      scanner.nextLine();
+
+      if (opcao == 1) {
+        System.out.print("Entre com o nome da pessoa candidata: ");
+        String nome = scanner.nextLine();
+        System.out.print("Entre com o número da pessoa candidata: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        gerenciamentoVotacao.cadastrarPessoaCandidata(nome, numero);
+      } else if (opcao == 2) {
+        break;
+      } else {
+        System.out.println("Opção inválida! Por favor, entre com 1 ou 2.");
+      }
+    }
+
+    System.out.println("\nCadastro de pessoas eleitoras:");
+    while (true) {
+      System.out.println("Cadastrar pessoa eleitora?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.print("Entre com o número correspondente à opção desejada: ");
+      int opcao = scanner.nextInt();
+      scanner.nextLine();
+
+      if (opcao == 1) {
+        System.out.print("Entre com o nome da pessoa eleitora: ");
+        String nome = scanner.nextLine();
+        System.out.print("Entre com o cpf da pessoa eleitora: ");
+        String cpf = scanner.nextLine();
+
+        gerenciamentoVotacao.cadastrarPessoaEleitora(nome, cpf);
+      } else if (opcao == 2) {
+        break;
+      } else {
+        System.out.println("Opção inválida! Por favor, entre com 1 ou 2.");
+      }
+    }
+
+    System.out.println("\nVotação:");
+    while (true) {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Votar");
+      System.out.println("2 - Resultado Parcial");
+      System.out.println("3 - Finalizar Votação");
+      int opcao = scanner.nextInt();
+
+      if (opcao == 1) {
+        scanner.nextLine();
+        System.out.print("Entre com o cpf da pessoa eleitora: ");
+        String cpf = scanner.nextLine();
+        System.out.print("Entre com o número da pessoa candidata: ");
+        int numero = scanner.nextInt();
+
+        gerenciamentoVotacao.votar(cpf, numero);
+      } else if (opcao == 2) {
+        gerenciamentoVotacao.mostrarResultado();
+      } else if (opcao == 3) {
+        System.out.println("Votação finalizada.");
+        gerenciamentoVotacao.mostrarResultado();
+        break;
+      } else {
+        System.out.println("Opção inválida! Por favor, entre com 1, 2 ou 3.");
+      }
+    }
+    scanner.close();
   }
-
 }
