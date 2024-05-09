ac526607663cb6ef6108ec72890b99a9dd3b7492
brandaorichard <rih.brandao@gmail.com>
2024-05-08 21:07:02 -0300

    fix: indentation & javadocs

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,29 +1,38 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Candidate Person Class which is an inheritance of Person.
+ */
 public class PessoaCandidata extends Pessoa {
-    private int numero;
-    private int votos;
+  private int numero;
+  private int votos;
 
-    public PessoaCandidata(String nome, int numero) {
-        super();
-        this.nome = nome;
-        this.numero = numero;
-        this.votos = 0;
-    }
+  /**
+   * Construtor da classe PessoaCandidata.
+   *
+   * @param nome Person object name
+   * @param numero Person object numero
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super();
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
 
-    public int getNumero() {
-        return numero;
-    }
+  public int getNumero() {
+    return numero;
+  }
 
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
 
-    public int getVotos() {
-        return votos;
-    }
+  public int getVotos() {
+    return votos;
+  }
 
-    public void receberVoto() {
-        votos++;
-    }
+  public void receberVoto() {
+    votos++;
+  }
 }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,19 +1,28 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Voter Person Class which is inherited from Pessoa.
+ */
 public class PessoaEleitora extends Pessoa {
-    private String cpf;
+  private String cpf;
 
-    public PessoaEleitora(String nome, String cpf) {
-        super();
-        this.nome = nome;
-        this.cpf = cpf;
-    }
+  /**
+   * Constructor of the PessoaEleitora class.
+   *
+   * @param nome Person object name
+   * @param cpf Person object CPF
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    super();
+    this.nome = nome;
+    this.cpf = cpf;
+  }
 
-    public String getCpf() {
-        return cpf;
-    }
+  public String getCpf() {
+    return cpf;
+  }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
